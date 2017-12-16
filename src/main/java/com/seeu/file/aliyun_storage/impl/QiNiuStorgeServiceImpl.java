2b2ec7e1d52e50f09a888e3afeae7e6e208ef940
xiaoyi <neoxiaoyi@163.com>
2017-12-16 20:32:13 +0800

    将阿里OSS更新为七牛云存储服务

--- src/main/java/com/seeu/file/aliyun_storage/impl/QiNiuStorgeServiceImpl.java
+++ src/main/java/com/seeu/file/aliyun_storage/impl/QiNiuStorgeServiceImpl.java
@@ -1,39 +1,32 @@
 package com.seeu.file.aliyun_storage.impl;
 
-import com.aliyun.oss.OSSClient;
+import com.google.gson.Gson;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
 import com.seeu.file.aliyun_storage.StorageImageService;
-import com.seeu.system.aliyunSdk.AliyunOSSUtil;
 import com.seeu.ywq.release.model.Image;
-import org.apache.commons.fileupload.disk.DiskFileItem;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-//@Service  暂时用七牛了。。
-@Deprecated
-public class AliImageStorgeServiceImpl implements StorageImageService {
-
-    @Value("${bucketName}")
-    private String BACKET_NAME;
-
+@Service
+public class QiNiuStorgeServiceImpl implements StorageImageService {
     @Override
     public Result saveImages(MultipartFile[] files) throws Exception {
-
-        OSSClient client = aliyunOSSUtil.getOSSClient();
         List<List<String>> urlList = new ArrayList<>();
         List<int[]> imagePXList = new ArrayList<>();
+
         for (MultipartFile file : files) {
             // 获取宽高
             BufferedImage bufferedImg = ImageIO.read(file.getInputStream());
@@ -41,19 +34,10 @@ public class AliImageStorgeServiceImpl implements StorageImageService {
             int imgHeight = bufferedImg.getHeight();
             imagePXList.add(new int[]{imgWidth, imgHeight});
 
-            // 上传阿里云
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-            String dir = dateFormat.format(new Date());
-            String folder = "ywq" + "/" + dir + "/";
-            String fileName = file.getOriginalFilename();
-            String suffix = fileName.contains(".") ? fileName.substring(fileName.lastIndexOf('.')) : "";
-            fileName = "YGQImage_" + UUID.randomUUID() + suffix;
-            //保存图片
-            List<String> urls = aliyunOSSUtil.uploadObject2OSS(client, file, BACKET_NAME, folder, fileName);
+            // 上传QiNiu
+            List<String> urls = upload(file.getBytes());
             urlList.add(urls);
         }
-        client.shutdown();//释放资源
-
         // 存储图片 URL 等基本信息到数据库
         List<Image> images = new ArrayList<>();
         for (int i = 0; i < urlList.size(); i++) {
@@ -94,7 +78,35 @@ public class AliImageStorgeServiceImpl implements StorageImageService {
         return result;
     }
 
-    @Autowired
-    AliyunOSSUtil aliyunOSSUtil;
+    private List<String> upload(byte[] inputBytes) throws Exception {
+        String key = "ywq" + UUID.randomUUID();
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone0());
+        UploadManager uploadManager = new UploadManager(cfg);
+        String accessKey = "ceLw-DN7neXckVebuUE6eOJC1GPBuXkXS4mkyOzb";
+        String secretKey = "pg3QL2EuJY9LOjSayhBWGHvAlwfdxEk9UXrx9zT9";
+        String bucket = "zspwork";
+        ByteArrayInputStream byteInputStream = new ByteArrayInputStream(inputBytes);
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
 
+        Response response = uploadManager.put(byteInputStream, key, upToken, null, null);
+        //解析上传成功的结果
+        DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+        System.out.println(putRet.key);
+        System.out.println(putRet.hash);
+        List<String> urls = new ArrayList<>();
+        String host = "http://o7k6tx0fl.bkt.clouddn.com/";
+        urls.add(host + key);// 原图
+        urls.add(host + key +"?imageView2/2/w/100");// 缩略图100
+        urls.add(host + key +"?imageView2/2/w/200");// 缩略图200
+        urls.add(host + key +"?imageView2/2/w/300");// 缩略图300
+        urls.add(host + key +"?imageView2/2/w/500");// 缩略图500
+        urls.add(host + key +"?imageMogr2/blur/40x50");// 原图 hazy
+        urls.add(host + key +"?imageMogr2/blur/40x50&imageView2/2/w/100");// 缩略图100
+        urls.add(host + key +"?imageMogr2/blur/40x50&imageView2/2/w/200");// 缩略图200
+        urls.add(host + key +"?imageMogr2/blur/40x50&imageView2/2/w/300");// 缩略图300
+        urls.add(host + key +"?imageMogr2/blur/40x50&imageView2/2/w/500");// 缩略图500
+        return urls;
+    }
 }
