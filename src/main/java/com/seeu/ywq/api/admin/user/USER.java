c987cf209e9ff637567739262da3d72a12e979a3
neo <neoxiaoyi@163.com>
2018-01-19 11:21:52 +0800

    管理員用戶接口

--- src/main/java/com/seeu/ywq/api/admin/user/USER.java
+++ src/main/java/com/seeu/ywq/api/admin/user/USER.java
@@ -1,18 +1,15 @@
-package com.seeu.ywq.api.admin.user
+package com.seeu.ywq.api.admin.user;
 
 /**
  * Created by suneo.
  * User: neo
  * Date: 19/01/2018
- * Time: 10:37 AM
+ * Time: 11:21 AM
  * Describe:
- */  
+ */
 
-enum class USER{
+public enum USER {
     phone,
     wechat,
     introduce
-}
-enum class A{
-
 }
