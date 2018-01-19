4d36fc34648b3ab544e479dad2f6f7aed85dd811
neo <neoxiaoyi@163.com>
2018-01-19 17:33:06 +0800

    用戶、短信、視頻解鎖

---
+++ src/main/java/com/seeu/ywq/api/admin/picture/PictureUploadApi.java
@@ -0,0 +1,40 @@
+package com.seeu.ywq.api.admin.picture;
+
+import com.seeu.core.R;
+import com.seeu.third.filestore.FileUploadService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * Created by suneo.
+ * User: neo
+ * Date: 19/01/2018
+ * Time: 5:19 PM
+ * Describe:
+ */
+
+
+@Api(tags = "图片上传接口", description = "上传图片，返回 URL")
+@RestController("adminPictureUploadApi")
+@RequestMapping("/api/admin/v1/picture")
+public class PictureUploadApi {
+
+    @Autowired
+    private FileUploadService fileUploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity upload(MultipartFile image) {
+        try {
+            return ResponseEntity.ok(fileUploadService.uploadImage(image));
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body(R.code(500).message("上传失败！"));
+        }
+    }
+}
--- src/main/java/com/seeu/ywq/api/release/utils/ImageUploadApi.java
+++ src/main/java/com/seeu/ywq/api/release/utils/ImageUploadApi.java
@@ -1,5 +1,6 @@
 package com.seeu.ywq.api.release.utils;
 
+import com.qiniu.util.Auth;
 import com.seeu.core.R;
 import com.seeu.third.filestore.FileUploadService;
 import com.seeu.ywq.resource.model.Image;
@@ -7,8 +8,10 @@ import com.seeu.ywq.resource.service.ImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,9 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Api(tags = "文件存取2", description = "七牛云服务")
+@Api(tags = "七牛云服务", description = "文件存取2/七牛 TOKEN 获取")
 @RestController
-@RequestMapping("/api/v1/upload/image")
+@RequestMapping("/api/v1")
 @PreAuthorize("hasRole('USER')")
 public class ImageUploadApi {
     @Autowired
@@ -30,7 +33,7 @@ public class ImageUploadApi {
     private ImageService imageService;
 
     @ApiOperation(value = "普通图片上传")
-    @PostMapping
+    @PostMapping("/upload/image")
     public ResponseEntity upload(MultipartFile file) {
         try {
             String url = fileUploadService.upload(file);
@@ -44,7 +47,7 @@ public class ImageUploadApi {
     }
 
     @ApiOperation(value = "带缩略图的图片上传")
-    @PostMapping("/with-thumb")
+    @PostMapping("/upload/image/with-thumb")
     public ResponseEntity uploadFull(MultipartFile file) {
         try {
             Image image = fileUploadService.uploadImage(file);
@@ -57,4 +60,26 @@ public class ImageUploadApi {
         }
     }
 
+
+    @Value("${qiniu.accessKey}")
+    private String accessKey;
+
+    @Value("${qiniu.secretKey}")
+    private String secretKey;
+
+    @Value("${qiniu.bucket}")
+    private String bucket;
+
+    @ApiOperation(value = "获取上传文件时需要的 TOKEN")
+    @GetMapping("/qiuniu/token")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity getQiniuToken() {
+        // 简单处理，不用service了。
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+        if (upToken == null) return ResponseEntity.status(500).body(R.code(500).message("获取失败，服务器异常"));
+        Map map = new HashMap();
+        map.put("token", upToken);
+        return ResponseEntity.ok(map);
+    }
 }
