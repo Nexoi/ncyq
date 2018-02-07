7bf81727c8cbeedc085424eeb5644a6b12ed7e8e
neo <neoxiaoyi@163.com>
2018-02-08 02:26:33 +0800

    last update 管理员权限加入

--- src/main/java/com/seeu/ywq/admin/service/impl/BindUserServiceImpl.java
+++ src/main/java/com/seeu/ywq/admin/service/impl/BindUserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -50,7 +51,11 @@ public class BindUserServiceImpl implements BindUserService {
 
     @Override
     public BindUser bind(Long adminUid, Long userUid) {
-        return null;
+        BindUser user = new BindUser();
+        user.setAdminUid(adminUid);
+        user.setNickUid(userUid);
+        user.setCreateTime(new Date());
+        return repository.save(user);
     }
 
     @Override
